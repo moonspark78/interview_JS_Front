6c5e1380c9123ee784e310265885a216957ccd07
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-10-31 17:07:17 +0100

    question 1

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 console.log("hello world!");
 
+
+// Question 1 -
+let count = 0;
+const nums = [0, 1, 2, 3];
+
+nums.forEach(num=> {
+    if(num){
+        count = count + 1;
+    }
+})
+
+/* Ici la valeur sera "3" parce que elle verifie si "num" est vrai,
+ sauf que pour "0"  le premier element vaut "fasle"   */
+console.log("La valeur de count est  ",count);
+
