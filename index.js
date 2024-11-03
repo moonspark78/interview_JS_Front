67a80e03df8c449e014b9e8262c9e6cab7fae402
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-11-03 19:18:34 +0100

    📈 question 3  📈

--- index.js
+++ index.js
@@ -22,5 +22,24 @@ console.log("La valeur de count est  ",count);
 // Question 2 -
 // que fait la méthode slice
 const arr = [2,3,4,5]
-console.log(arr);
-console.log(arr.slice(0,1)); //[2]
+//console.log(arr);
+//console.log(arr.slice(0,1)); //[2]
+arr.splice(0,1)
+arr.unshift(8);
+console.log(arr);  //[8,3,4,5]
+
+
+
+
+
+// Question 3
+
+const obj ={
+    a: 1,
+    b:2,
+    c:3,
+    "a":6,
+    b:7,
+}
+// Whats the output ?
+console.log("is. :  ",obj);
